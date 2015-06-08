1be463c74ee35abed927c652522a893d94deb3f0
tonywr71 <tonywr71@gmail.com>
2015-06-08 11:21:21 +0000

    blah

--- test/server/jobs-data-spec.js
+++ test/server/jobs-data-spec.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
-var jobModel = require('../models/Job');
+var jobModel = require('../../models/Job');
 var Promise = require('bluebird');
-var jobsData = require("../jobs-data.js");
+var jobsData = require("../../jobs-data.js");
 
 function resetJobs(cb) {
     return new Promise(function(resolve, reject) {
@@ -25,6 +25,10 @@ describe("get jobs", function(){
             });     
     });
     
+    after(function() {
+        mongoose.connection.close();
+    })
+    
     it("should never be empty since jobs are seeded", function(){
         expect(jobs.length).to.be.at.least(1);
     });
@@ -38,3 +42,33 @@ describe("get jobs", function(){
     });
 
 });
+
+describe("db save jobs", function() {
+    
+    var job = {title:'Cook', description:'You will be making bagels'};
+    var jobs;
+    
+    function saveTestJob() {
+        return jobsData.saveJob(job);
+    }
+    
+    before(function(done) {
+         jobsData.connectDB('mongodb://localhost/jobfinder')
+            .then(resetJobs)
+            .then(function() { return jobsData.saveJob(job) })
+            .then(jobsData.findJobs)
+            .then(function setJobs(collection){
+                jobs = collection;
+                done();
+            });     
+    });
+    
+    after(function() {
+        mongoose.connection.close();
+    })
+    
+    it("should have one job after saving one job", function() {
+        expect(jobs).to.have.length(1);
+    })
+    
+})
